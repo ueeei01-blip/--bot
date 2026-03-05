68a871ec5664ee70592a445c1678d84b1b985bfd
ueeei01-blip <ueeei01@gmail.com>
2026-03-06 01:24:54 +0900

    Update main.mjs

--- main.mjs
+++ main.mjs
@@ -152,6 +152,10 @@ Legendary : ${h.Legendary}`
   }
 });
 
+client.once("ready", () => {
+  console.log(`✅ Logged in as ${client.user.tag}`);
+});
+
 client.login(process.env.DISCORD_TOKEN);
 
 import http from "http";
